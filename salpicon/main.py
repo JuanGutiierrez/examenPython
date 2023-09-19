c6f350730a721a607ff0553ffdcabae53e1670d2
JuanGutiierrez <joselitoguti2003@gmail.com>
2023-09-19 08:46:14 -0500

    feat: creamos menu y sus opciones en la carpeta festival

--- salpicon/main.py
+++ salpicon/main.py
@@ -8,6 +8,7 @@ print("4. Fruta mas cara: ")
 print("5. Salir")
 
 frutas = []
+frutas.sort(reverse=True)
 while opcion != 5:
     fruta={}
     opcion=int(input("Digita la opción deseada: "))
@@ -15,13 +16,21 @@ while opcion != 5:
         print("\n**** Ingresa nuevo producto ****")
         fruta["codigo"] = int(input("Ingresa el ID de la fruta: "))
         fruta["nombre"] = input("Ingresa nombre de la fruta: ")
-        fruta["descripcion"] = float(input("Ingresa el precio unitario de la fruta: "))
-        fruta["foto"] = input("Ingrese la cantidad: ")
+        fruta["precioUnitario"] = float(input("Ingresa el precio unitario de la fruta: "))
+        fruta["cantidad"] = input("Ingrese la cantidad: ")
         frutas.append(fruta)
     elif opcion == 2:
-        pass
+        
+        print("\n**** Mostrando frutas ****")
+        for frutaSeleccionada in frutas:
+            print (f"Codigo = {frutaSeleccionada['codigo']}")
+            print(f"Nombre = {frutaSeleccionada['nombre']}")
+            print(f"Descripcion =  {frutaSeleccionada['precioUnitario']}")
+            print(f"esta es la cantidad de la fruta ingresada = {frutaSeleccionada['cantidad']}")
+        
     elif opcion == 3:
-        pass
+        costoTotal = ['precioUnitario'] * ['cantidad']
+        print(f"El consto total es: {costoTotal}")
     elif opcion == 4:
         pass
     elif opcion == 5:
